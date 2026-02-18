3c52050cd6f72a1150b819078dc21515d587a915
bhanukamd1233-cyber <bhanukamd1233@gmail.com>
2026-02-18 21:20:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "30pj2ZyS#BfM1f9lsGAjeSCTmdb-LQlafsMupqli34KKS7bZ1o2s",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/bhanukamd1233-cyber/Bhanuka_MD/blob/main/images/alive%20msg.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 BHANUKA-MD Is Alive Now😍*",
 BOT_OWNER: '94724258960',  // Replace with the owner's phone number
